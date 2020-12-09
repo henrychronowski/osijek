2b4c1be7e5b7a4364f634455c3c0b9bda68d3acf
henrychronowski <hrchronowski@gmail.com>
2020-12-09 14:02:33 -0500

    Adding initial program_options implementation

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ SRC     := src
 INCLUDE := include
 LIB := lib
 
-LIBRARIES   := 
+LIBRARIES   := -lboost_program_options
 EXECUTABLE  := delete_driver
 
 SO_NAME := diskDelete
--- src/delete_driver/main.cpp
+++ src/delete_driver/main.cpp
@@ -6,18 +6,44 @@
  * @assignment Final Project
  * @date 07/12/2020
  * @credits  
+ * https://www.boost.org/doc/libs/1_63_0/doc/html/program_options.html
  * 
  **/
 
 
 #include "diskDelete.h"
 #include <iostream>
+
+#include <boost/program_options.hpp>
+
+using namespace boost::program_options;
   
-int main()
+int main(int argc, const char *argv[])
 {
-    const char* disk = "/dev/sda";
-    bool result;
-    checkForRoot(disk);
-    result = wipeDisk(disk, 512);
+    bool result = EXIT_SUCCESS;
+
+    options_description desc{"Options"};
+    desc.add_options()
+      ("help,h", "Help screen")
+      ("log,l", value<std::string>()->default_value("log.txt"), "Log")
+      ("disk,d", value<std::string>()->default_value("/dev/sda"), "Disk file");
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+    notify(vm);
+
+    if (vm.count("help"))
+      std::cout << desc << '\n';
+    else
+    {
+        const char* logFile = vm["log"].as<const char*>();
+        const char* disk = vm["disk"].as<const char*>();
+
+        const char* disk = "/dev/sda";
+        result = checkForRoot(disk);
+        //result = wipeDisk(disk, 512);
+    }
+
+    
     return result;
 }
